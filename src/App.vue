3819c3c3b5fcf5deb23870cd3a62c0414cebac51
RaiqueGabriel <gabriel.raique@gmail.com>
2020-04-14 23:50:30 -0300

    atividade realizada com vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container"> 
+        <div class="tittle">
+          <h1>Registro de Livros</h1>
+        </div>
+        <div class="register-form">
+            <form class="needs-validation" @submit.prevent="addLivro(livro)">
+                <div class="form-row">
+                    <div class="col">
+                        <label for="validationCustom03">Titulo</label>
+                        <input type="text" class="form-control" v-model="livro.titulo" required>
+                    </div>
+                </div>
+                <div class="form-row">
+                    <div class="col">
+                        <label for="validationCustom01">Editora </label>
+                        <input type="text" class="form-control" v-model="livro.editora" required>
+                    </div>
+                    <div class="col">
+                        <label for="validationCustom02">Paginas</label>
+                        <input type="text" class="form-control" v-model="livro.paginas" required>
+                    </div>
+                </div>
+                <button class="btn btn-primary" type="submit" id="submit-button">Salvar</button>
+            </form>
+        </div>
+    </div>
+    <List v-for="l in livros" :key="l.id" :livro="l"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/List';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{ 
+    List 
+  },
+  data() {
+    return{
+      livros: [],
+      livro:{}
+    }
+  },
+  methods:{
+    addLivro(livro){
+      livro.id = Date.now();
+      this.livros.push(livro);
+      this.livro = {}
+    }
   }
 }
 </script>
@@ -24,5 +62,37 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.selector-for-some-widget {
+  box-sizing: content-box;
+}
+
+.register-form {
+    margin:0 auto; 
+    width:600px;
+    padding:20px;
+    background:#f9f9f9;
+    border:2px solid #333;
+}
+
+.list-form {
+    margin:0 auto; 
+    width:600px;
+    padding:20px;
+    background:#f9f9f9;
+    border:2px solid #333;
+}
+
+.container {
+    margin-bottom: 2%;
+}
+
+#submit-button {
+    margin-top: 2%;
+}
+
+ul {
+    list-style-type: none;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="list-form">
+      <div>
+        <h1>Lista de Livros</h1>
+      </div>
+      <div>
+        <ul class="list-group">
+          <li>Titulo: {{ livro.titulo }}, Editora {{ livro.editora }}, Paginas: {{ livro.paginas }} </li>
+        </ul>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        livro:{type: Object, required:true}
+    }
+}
+</script>
